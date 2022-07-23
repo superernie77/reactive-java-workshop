1d6024d294d0ea2314f5589df3c671eb2d0591f5
superernie77 <superernie77@gmail.com>
2022-07-23 17:18:42 +0200

    Exercise 1

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -7,23 +7,32 @@ public class Exercise1 {
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
 
         // Print all numbers in the intNumbersStream stream
-        // TODO: Write code here
+    	StreamSources.intNumbersStream().forEach( i -> System.out.println(i));
 
+    	System.out.println("-------------------------");
+    	
         // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
+    	StreamSources.intNumbersStream().filter( i -> i<5  ).forEach( i -> System.out.println(i));
 
+    	System.out.println("-------------------------");
+    	
         // Print the second and third numbers in intNumbersStream that's greater than 5
-        // TODO: Write code here
+    	StreamSources.intNumbersStream().filter( i -> i > 5  ).skip(1).limit(2).forEach( i -> System.out.println(i));
 
+    	System.out.println("-------------------------");
+    	
         //  Print the first number in intNumbersStream that's greater than 5.
-        //  If nothing is found, print -1
-        // TODO: Write code here
+    	System.out.println( StreamSources.intNumbersStream().filter( i -> i > 5  ).findFirst().orElse(-1));
 
+    	System.out.println("-------------------------");
+    	
         // Print first names of all users in userStream
-        // TODO: Write code here
+    	StreamSources.userStream().map( u -> u.getFirstName()).forEach( n -> System.out.println(n));
 
+    	System.out.println("-------------------------");
+    	
         // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
+    	StreamSources.userStream().filter( u ->  StreamSources.intNumbersStream().anyMatch( i -> u.getId() == i ) ).forEach( u -> System.out.println(u.getFirstName()));
 
     }
 
