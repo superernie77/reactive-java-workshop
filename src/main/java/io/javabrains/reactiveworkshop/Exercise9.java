d77a95e62c492493836a3eb5782293012ecb015d
superernie77 <superernie77@gmail.com>
2022-10-19 08:08:15 +0200

    Collectors examples

--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -10,13 +10,13 @@ public class Exercise9 {
         // Use ReactiveSources.intNumbersFlux()
 
         // Print size of intNumbersFlux after the last item returns
-        // TODO: Write code here
+    	//ReactiveSources.intNumbersFlux().count().subscribe( i -> System.out.println(i));
 
         // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
+    	//ReactiveSources.intNumbersFlux().collectList().subscribe( i -> System.out.println(i));
 
         // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+    	ReactiveSources.intNumbersFlux().buffer(2).map( l -> l.get(0)+ l.get(1)).subscribe(s -> System.out.println(s));
 
         System.out.println("Press a key to end");
         System.in.read();
